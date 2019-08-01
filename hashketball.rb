1aeca26788055d886f4e00a63edef16a23dadaeb
Marisa Canan-Zucker <mcananzucker@gmail.com>
2019-08-01 21:58:43 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -7,7 +7,7 @@ def game_hash
      :away => { :team_name => "Charlotte Hornets",
                 :colors => ["Turquoise", "Purple"],
                 :players => [
-                  "Jeff Adrien" = {
+                  {:player_name => "Jeff Adrien",
                    :number => 4,
                    :shoe => 18,
                    :points => 10,
@@ -17,7 +17,7 @@ def game_hash
                    :blocks => 7,
                    :slam_dunks => 2
                   },
-                  "Bismack Biyombo" = {
+                  {:player_name => "Bismack Biyombo",
                    :number => 0,
                    :shoe => 16,
                    :points => 12,
@@ -27,7 +27,7 @@ def game_hash
                    :blocks => 15,
                    :slam_dunks => 10
                   },
-                  "DeSagna Diop" = {
+                  {:player_name => "DeSagna Diop",
                    :number => 2,
                    :shoe => 14,
                    :points => 24,
@@ -37,7 +37,7 @@ def game_hash
                    :blocks => 5,
                    :slam_dunks => 5
                   },
-                  "Ben Gordon" = {
+                  {:player_name => "Ben Gordon",
                    :number => 8,
                    :shoe => 15,
                    :points => 33,
@@ -47,7 +47,7 @@ def game_hash
                    :blocks => 1,
                    :slam_dunks => 0
                   },
-                  "Kemba Walker" = {
+                  {:player_name => "Kemba Walker",
                    :number => 33,
                    :shoe => 15,
                    :points => 6,
@@ -62,7 +62,7 @@ def game_hash
      :home => { :team_name => "Brooklyn Nets",
                 :colors => ["Black", "White"],
                 :players => [
-                  "Alan Anderson" = {
+                  {:player_name => "Alan Anderson",
                    :number => 0,
                    :shoe => 16,
                    :points => 22,
@@ -72,7 +72,7 @@ def game_hash
                    :blocks => 1,
                    :slam_dunks => 1
                   },
-                  "Reggie Evans" = {
+                  {:player_name => "Reggie Evans",
                    :number => 30,
                    :shoe => 14,
                    :points => 12,
@@ -82,7 +82,7 @@ def game_hash
                    :blocks => 12,
                    :slam_dunks => 7
                   },
-                  "Brook Lopez" = {
+                  {:player_name => "Brook Lopez",
                    :number => 11,
                    :shoe => 17,
                    :points => 17,
@@ -92,7 +92,7 @@ def game_hash
                    :blocks => 1,
                    :slam_dunks => 15
                   },
-                  "Mason Plumlee" = {
+                  {:player_name => "Mason Plumlee",
                    :number => 1,
                    :shoe => 19,
                    :points => 26,
@@ -102,7 +102,7 @@ def game_hash
                    :blocks => 8,
                    :slam_dunks => 5
                   },
-                  "Jason Terry" = {
+                  {:player_name => "Jason Terry",
                    :number => 31,
                    :shoe => 15,
                    :points => 19,
@@ -120,8 +120,24 @@ def game_hash
 
 
 ###########
+def player_card(name)
+    
+end
 
-
+def num_points_scored(player_pts)
+  
+  game_hash.each do |loc, team|
+    team.each do |feature, info|
+      if feature == :players
+        info.each do |players|
+          if players[:player_name] == player
+            player[:points]
+          end
+        end
+      end
+    end
+  end
+end
   
   
 
